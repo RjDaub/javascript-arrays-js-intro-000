220a3432346647d477ee3d460daa0c184c2e09b2
Robert Daub <rjd2066@gmail.com>
2020-04-05 18:27:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,27 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+
+
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
+  addElementToBeginningOfArray();
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
-  return array
-}
 
-function descructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element)
 
-  return array
-}
-function addElementToEndOfArray(array, element) {
-  return [...array, element]
-}
-function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(element)
-  return array
-}
-function accessElementInArray(array, index) {
-  return array[index]
-}
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift()
