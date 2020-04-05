62f8feb8a4742e5a8c27ce03eb24fd582db57c5a
Robert Daub <rjd2066@gmail.com>
2020-04-05 18:50:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,44 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
-return chocolateBars;
 
-function addElementToBeginningOfArray(array = Name, element) {
-  element = ['Rj', 'Jessie']
-  return ['joey'. ...element]
+
+
+
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function descructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index) {
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
 }
